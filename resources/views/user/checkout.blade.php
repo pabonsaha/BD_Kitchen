835fd8cf0618aacb28689cf7b173ffa911f4c95d
pabonsaha <pabonsu@outlook.com>
2024-11-09 11:55:20 +0600

    done

--- resources/views/user/checkout.blade.php
+++ resources/views/user/checkout.blade.php
@@ -5,116 +5,131 @@
     <main class="mb-[60px]">
         <!-- populer kitchen description start  -->
         <section class="container mx-auto mt-[0px] text-center">
-            <h4 class="text-[#E32938] capitalize inter-700 text-2xl text-wrap md:text-3xl lg:text-4xl m-[40px]">Checkout</h4>
+            <h4 class="text-[#E32938] capitalize inter-700 text-2xl text-wrap md:text-3xl lg:text-4xl m-[40px]">
+                Checkout</h4>
         </section>
         <form method="POST" action="{{route('order.store')}}">
             @csrf
             <section class="container mx-auto mt-[40px]">
-            <div class="grid grid-cols-2">
+                <div class="grid grid-cols-2">
 
-                <input class="hidden" name="shipping_charge" value="{{$shop->delivery_charge}}">
-                <input class="hidden" name="seller_id" value="{{$shop->user_id}}">
+                    <input class="hidden" name="shipping_charge" value="{{$shop->delivery_charge}}">
+                    <input class="hidden" name="seller_id" value="{{$shop->user_id}}">
 
-                <div class="product_id">
-                    <p class="capitalize inter-700 text-xl text-wrap  mb-[40px]">Product List</p>
-                    @php
-                        $subTotal = 0;
-                    @endphp
-                    @foreach ($products as $product)
+                    <div class="product_id">
+                        <p class="capitalize inter-700 text-xl text-wrap  mb-[40px]">Product List</p>
+                        @php
+                            $subTotal = 0;
+                        @endphp
+                        @foreach ($products as $product)
 
-                        <input class="hidden" name="product[{{$product->id}}][id]" value="{{$product->id}}">
-                        <input class="hidden" name="product[{{$product->id}}][price]" value="{{$product->unit_price}}">
-                        <input class="hidden" name="product[{{$product->id}}][quantity]" value="{{$product->quantity_value}}">
+                            <input class="hidden" name="product[{{$product->id}}][id]" value="{{$product->id}}">
+                            <input class="hidden" name="product[{{$product->id}}][price]"
+                                   value="{{$product->unit_price}}">
+                            <input class="hidden" name="product[{{$product->id}}][quantity]"
+                                   value="{{$product->quantity_value}}">
 
-                        <div
-                            class="mb-[15px] h-[120px] flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                            <img class="object-contain w-full rounded-t-lg h-36 md:w-48 md:rounded-none md:rounded-s-lg"
-                                 src="{{ asset('storage/' . $product->thumbnail_img) }}" alt="">
-                            <div class="flex flex-col justify-between p-4 leading-normal w-full">
-                                <h5 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                    {{ $product->name }}</h5>
-                                <div class="flex flex-row justify-between">
-                                    <h4 class="font-semibold text-[#E32938]">{{ getPriceFormat($product->unit_price) }} *
-                                        {{$product->quantity_value}}</h4>
-                                    <div class="hidden product_details_container">
-                                        <input class="product_id" value="{{$product->id}}">
-                                        <input class="product_name" value="{{$product->name}}">
-                                        <input class="product_image" value="{{ asset('storage/' . $product->thumbnail_img) }}">
-                                        <input class="product_price" value="{{$product->unit_price}}">
+                            <div
+                                class="mb-[15px] h-[120px] flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img
+                                    class="object-contain w-full rounded-t-lg h-36 md:w-48 md:rounded-none md:rounded-s-lg"
+                                    src="{{ asset('storage/' . $product->thumbnail_img) }}" alt="">
+                                <div class="flex flex-col justify-between p-4 leading-normal w-full">
+                                    <h5 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                        {{ $product->name }}</h5>
+                                    <div class="flex flex-row justify-between">
+                                        <h4 class="font-semibold text-[#E32938]">{{ getPriceFormat($product->unit_price) }}
+                                            *
+                                            {{$product->quantity_value}}</h4>
+                                        <div class="hidden product_details_container">
+                                            <input class="product_id" value="{{$product->id}}">
+                                            <input class="product_name" value="{{$product->name}}">
+                                            <input class="product_image"
+                                                   value="{{ asset('storage/' . $product->thumbnail_img) }}">
+                                            <input class="product_price" value="{{$product->unit_price}}">
+                                        </div>
+                                        <h4 class="font-semibold text-[#E32938]">{{ getPriceFormat($product->unit_price* $product->quantity_value) }}</h4>
+
                                     </div>
-                                    <h4 class="font-semibold text-[#E32938]">{{ getPriceFormat($product->unit_price* $product->quantity_value) }}</h4>
-
                                 </div>
                             </div>
+                            @php
+                                $subTotal += ((int)$product->unit_price*$product->quantity_value);
+                            @endphp
+                        @endforeach
+                        <div
+                            class="mt-[20px] flex flex-row justify-between   md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <p class="font-semibold">Subtotal</p>
+                            <p class="text-[#E32938] font-semibold">{{getPriceFormat($subTotal)}}</p>
+                            <input name="sub_total" class="hidden" value="{{$subTotal}}">
                         </div>
-                        @php
-                            $subTotal += (int)$product->unit_price;
-                        @endphp
-                    @endforeach
-                    <div class="mt-[20px] flex flex-row justify-between   md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <p class="font-semibold">Subtotal</p>
-                        <p class="text-[#E32938] font-semibold">{{getPriceFormat($subTotal)}}</p>
-                        <input name="sub_total" class="hidden" value="{{$subTotal}}">
+                        <div
+                            class="mt-[10px] flex flex-row justify-between   md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <p class="font-semibold">Delivery Charge</p>
+                            <p class="text-[#E32938] font-semibold">{{getPriceFormat($shop->delivery_charge)}}</p>
+                        </div>
+                        <div
+                            class="mt-[10px] flex flex-row justify-between   md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <p class="font-semibold">Total</p>
+                            <p class="text-[#E32938] font-semibold">{{getPriceFormat($subTotal+$shop->delivery_charge)}}</p>
+                            <input name="total" class="hidden" value="{{$subTotal+$shop->delivery_charge}}">
+
+                        </div>
                     </div>
-                    <div class="mt-[10px] flex flex-row justify-between   md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <p class="font-semibold">Delivery Charge</p>
-                        <p class="text-[#E32938] font-semibold">{{getPriceFormat($shop->delivery_charge)}}</p>
-                    </div>
-                    <div class="mt-[10px] flex flex-row justify-between   md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <p class="font-semibold">Total</p>
-                        <p class="text-[#E32938] font-semibold">{{getPriceFormat($subTotal+$shop->delivery_charge)}}</p>
-                        <input name="total" class="hidden" value="{{$subTotal+$shop->delivery_charge}}">
+
+                    <div class="cart_list">
+                        <p class="capitalize inter-700 text-xl text-wrap mb-[12px]">Shipping Address</p>
+
+
+                        <div class="form">
+                            <label for="Name">Name</label>
+                            <input class="inputForm" required value="{{old('name')}}" type="text"
+                                   name="shipping_addresses_name">
+                            @error('name')
+                            <span class="error">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+
+                        <div class="form">
+                            <label for="Name">Phone Number</label>
+                            <input class="inputForm" required type="tel" value="{{old('phone')}}"
+                                   name="shipping_addresses_phone">
+                            @error('phone')
+                            <span class="error">{{ $message }}</span>
+                            @enderror
+
+                        </div>
+
+                        <div class="form">
+                            <label for="Name">Street Address</label>
+                            <input class="inputForm" required value="{{old('name')}}" type="text"
+                                   name="shipping_addresses_street_address">
+                            @error('name')
+                            <span class="error">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form">
+                            <label for="Name">City</label>
+                            <input class="inputForm" required value="{{old('name')}}" type="text"
+                                   name="shipping_addresses_city">
+                            @error('name')
+                            <span class="error">{{ $message }}</span>
+                            @enderror
+                        </div>
+
 
                     </div>
                 </div>
 
-                <div class="cart_list">
-                    <p class="capitalize inter-700 text-xl text-wrap mb-[12px]">Shipping Address</p>
-
-
-                            <div class="form">
-                                <label for="Name">Name</label>
-                                <input class="inputForm" required value="{{old('name')}}" type="text" name="shipping_addresses_name">
-                                @error('name')
-                                <span class="error">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-
-                            <div class="form">
-                                <label for="Name">Phone Number</label>
-                                <input class="inputForm" required type="tel" value="{{old('phone')}}" name="shipping_addresses_phone">
-                                @error('phone')
-                                <span class="error">{{ $message }}</span>
-                                @enderror
-
-                            </div>
-
-                    <div class="form">
-                        <label for="Name">Street Address</label>
-                        <input class="inputForm" required value="{{old('name')}}" type="text" name="shipping_addresses_street_address">
-                        @error('name')
-                        <span class="error">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="form">
-                        <label for="Name">City</label>
-                        <input class="inputForm" required value="{{old('name')}}" type="text" name="shipping_addresses_city">
-                        @error('name')
-                        <span class="error">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-
-
+                <div>
+                    <button
+                        class="mt-2 px-6 flex items-center btn text-[#E32938] border border-solid border-[#E3293880] bg-[#FCEAEB] m-auto"
+                        type="submit">Place Order
+                    </button>
                 </div>
-            </div>
 
-            <div>
-                <button class="mt-2 px-6 flex items-center btn text-[#E32938] border border-solid border-[#E3293880] bg-[#FCEAEB] m-auto" type="submit">Place Order</button>
-            </div>
-
-        </section>
+            </section>
 
         </form>
 
